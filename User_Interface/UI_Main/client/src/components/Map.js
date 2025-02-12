9bb7e0a5a46a1e1467329ace10f1a4b135f2fba5
Ryan Mathews <mathewsryan2@gmail.com>
2025-02-12 10:47:58 -0600

    Got layout for camera and telemtry feed, added background color, and Warning.

--- User_Interface/UI_Main/client/src/components/Map.js
+++ User_Interface/UI_Main/client/src/components/Map.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React from "react";
+import "./css/MapCSS.css";
 
 /*
-* Map() - **PAGE**
-*
-* Description:
-*      Page component where Map-specific items will be built
-*      and displayed, such as 2D map, LTV items, etc.
-*
-* Params:
-*     None
-*
-* Returns:
-*     A JSX object to be displayed.
-*/
+ * Map() - **PAGE**
+ *
+ * Description:
+ *      Page component where Map-specific items will be built
+ *      and displayed, such as 2D map, LTV items, etc.
+ *
+ * Params:
+ *     None
+ *
+ * Returns:
+ *     A JSX object to be displayed.
+ */
 function Map() {
-  return(
+  return (
     <div>
-      <h2>Map Page</h2>
-      <p>This is the Map Page!</p>
+      <div className="ParentContainerEVA">
+        <div className="EVA" id="container3">
+          <h1>EVA1</h1>
+        </div>
+        <div className="EVA" id="container4">
+          <h1>EVA2</h1>
+        </div>
+      </div>
+
+      <div className="ParentContainerLTV">
+        <div className="LTV" id="container1">
+          <h1>LTV1</h1>
+        </div>
+        <div className="LTV" id="container2">
+          <h1>LTV2</h1>
+        </div>
+      </div>
+
+      <div className="WarningContainer">
+        <h1>Warning</h1>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Map;
