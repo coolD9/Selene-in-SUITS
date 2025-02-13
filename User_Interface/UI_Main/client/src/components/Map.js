1b191d21a5c3308d1311512a4a14b39b27ba0ade
Ryan Mathews <mathewsryan2@gmail.com>
2025-02-12 22:14:25 -0600

    Changed layout to flex layout, added map and fixed warning sections

--- User_Interface/UI_Main/client/src/components/Map.js
+++ User_Interface/UI_Main/client/src/components/Map.js
@@ -16,27 +16,33 @@ import "./css/MapCSS.css";
  */
 function Map() {
   return (
-    <div>
-      <div className="ParentContainerEVA">
-        <div className="EVA" id="container3">
-          <h1>EVA1</h1>
+    <div className="body">
+      <div className="LeftContainer">
+        <div className="ParentContainerEVA">
+          <div className="EVA" id="container1">
+            <h1>EVA1</h1>
+          </div>
+          <div className="EVA" id="container2">
+            <h1>EVA2</h1>
+          </div>
         </div>
-        <div className="EVA" id="container4">
-          <h1>EVA2</h1>
+
+        <div className="ParentContainerLTV">
+          <div className="LTV" id="container3">
+            <h1>LTV1</h1>
+          </div>
+          <div className="LTV" id="container4">
+            <h1>LTV2</h1>
+          </div>
+        </div>
+
+        <div className="WarningContainer">
+          <h1>Warning</h1>
         </div>
       </div>
 
-      <div className="ParentContainerLTV">
-        <div className="LTV" id="container1">
-          <h1>LTV1</h1>
-        </div>
-        <div className="LTV" id="container2">
-          <h1>LTV2</h1>
-        </div>
-      </div>
-
-      <div className="WarningContainer">
-        <h1>Warning</h1>
+      <div className="mapcontainer">
+        <h1 id="map">Map</h1>
       </div>
     </div>
   );
