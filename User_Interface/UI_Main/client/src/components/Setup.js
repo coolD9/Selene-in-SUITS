6844fdcc8cded8c7bc40b7de3b02f876c8151e4b
coolD9 <123680618+coolD9@users.noreply.github.com>
2025-02-16 21:37:25 +0000

    Setup to GUI Branch

--- User_Interface/UI_Main/client/src/components/Setup.js
+++ User_Interface/UI_Main/client/src/components/Setup.js
@@ -18,6 +18,20 @@ function Setup() {
     <div>
         <h2>Setup Page</h2>
         <p>This is the Setup Page!</p>
+
+        <h2>Enter IP adressses</h2>
+       
+        <label>
+        DUST IP: <input name="myInput" defaultValue="00.0.0.000:0000" />
+        </label>
+
+        <p>DUST IP:</p>
+        <p>TSS IP:</p>
+
+        <p>EVA 1:</p>
+        <p>EVA 2:</p>
+        <p>LTV:</p>
+
     </div>
   )
 }
