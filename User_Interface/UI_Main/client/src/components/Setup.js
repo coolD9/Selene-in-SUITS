a1f2c07da15ce2d48ed51c57a549e943e39f5921
Jack Tsui <jacktsui23@gmail.com>
2025-02-18 23:49:18 +0000

    Made grayed-out placeholder text in the input boxes. They disappear when user starts to type the IP address instead of having to delete all of the example IP address.

--- User_Interface/UI_Main/client/src/components/Setup.js
+++ User_Interface/UI_Main/client/src/components/Setup.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import "./css/SetupCSS.css";
 
@@ -15,45 +16,52 @@ import "./css/SetupCSS.css";
 * Returns:
 *     A JSX object to be displayed.
 */
+
+
 function Setup() {
   return (
     <div className="setup-container">
       <h2>Enter IP addresses</h2>
-      
+
       {/* Form used for input fields.*/}
       <form className="ip-form">
-
         <div className="form-row">
+
+          {/* The label shows the text "DUST IP:" next to the input box*/}
           <label>DUST IP:</label>
-          <input name="dustInput" defaultValue="00.0.0.000:0000" />
+
+          
+            {/* This creates an input box where users can type.
+            name="dustInput" gives the input box a name to identify it.
+            placeholder="00.0.0.000:0000" shows grayed-out example text*/}
+          <input name="dustInput" placeholder="00.0.0.000:0000" />
         </div>
 
         <div className="form-row">
           <label>TSS IP:</label>
-          <input name="tssInput" defaultValue="00.0.0.000:0000" />
+          <input name="tssInput" placeholder="00.0.0.000:0000" />
         </div>
         
         <div className="form-row">
           <label>DUST IP:</label>
-          <input name="dustInput" defaultValue="00.0.0.000:0000" />
+          <input name="dustInput" placeholder="00.0.0.000:0000" />
         </div>
 
         <div className="form-row">
           <label>EVA 1:</label>
-          <input name="evaInput" defaultValue="00.0.0.000:0000" />
+          <input name="evaInput" placeholder="00.0.0.000:0000" />
         </div>
 
         <div className="form-row">
           <label>EVA 2:</label>
-          <input name="eva2Input" defaultValue="00.0.0.000:0000" />
+          <input name="eva2Input" placeholder="00.0.0.000:0000" />
         </div>
 
         <div className="form-row">
           <label>LTV:</label>
-          <input name="ltvInput" defaultValue="00.0.0.000:0000" />
+          <input name="ltvInput" placeholder="00.0.0.000:0000" />
         </div>
       </form>
-
     </div>
   );
 }
