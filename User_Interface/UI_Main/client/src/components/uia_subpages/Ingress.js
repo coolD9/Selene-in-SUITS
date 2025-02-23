c48093419d22095c134b48a73a8709b8f9858bcf
rjae5 <rykirjoe@yahoo.com>
2025-02-23 16:37:48 -0600

    Add images to ingress.js for later styling

--- User_Interface/UI_Main/client/src/components/uia_subpages/Ingress.js
+++ User_Interface/UI_Main/client/src/components/uia_subpages/Ingress.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import DCU_Front from './../images/DCU_Front.jpg'
+import UIA_Panel from './../images/SUITS_UIA_PANEL.jpg'
+import './../css/MasterCSS.css'
+import './../css/UIACSS.css'
 
 /*
 * Egress() - **PAGE**
@@ -15,8 +19,13 @@ import React from 'react';
 */
 function Ingress() {
   return(
-    <div>
-     <p> Ingress page </p>
+    <div className=''>
+      <div>
+        <img src={DCU_Front}></img>
+      </div>
+      <div>
+        <img src={UIA_Panel}></img>
+      </div>
     </div>
   )
 }
