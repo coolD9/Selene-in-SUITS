3dfb9edf71848b6d5e031fe73a36cd03f612348f
rjae5 <rykirjoe@yahoo.com>
2025-03-02 18:31:42 -0600

    Add preliminary status indicators in the monitor section

--- User_Interface/UI_Main/client/src/components/UIA.js
+++ User_Interface/UI_Main/client/src/components/UIA.js
@@ -33,6 +33,34 @@ function UIA() {
   return(
     <div className='theme_background'>
       <div className='monitor_container theme_light_border'>
+          <p className='theme_text theme_light_border monitor_title'>Status Monitor</p>
+          <div className='monitor_statuses'>
+            <div className='status_block'>
+              <p>EMU1 Power: Value</p>
+              <p>EV1 Supply: Value</p>
+              <p>EV1 Waste: Value</p>
+              <p>EV1 Oxygen: Value</p>
+              <p>O2 Vent: Value</p>
+            </div>
+            <div className='status_block'>
+              <p>EMU2 Power: Value</p>
+              <p>EV2 Supply: Value</p>
+              <p>EV2 Waste: Value</p>
+              <p>EV2 Oxygen: Value</p>
+              <o>Depress Pump: Value</o>
+
+            </div>
+            <div className='status_block'>
+              <p>Battery: Value</p>
+              <p>Oxygen: Value</p>
+              <p>Comms: Value</p>
+              <p>Fan: Value</p>
+              <p>Pump: Value</p>
+              <p>CO2: Value</p>
+            </div>
+          </div>
+          
+         
         
       </div>
       <div className='UIA_container  theme_text'>
