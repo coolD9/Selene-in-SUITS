62339df3f61362dda9918a31ffc4c76173d28a3a
rjae5 <rykirjoe@yahoo.com>
2025-03-23 16:19:45 -0500

    Import UIA and DCU json files from telemetry_json

--- User_Interface/UI_Main/client/src/components/UIA.tsx
+++ User_Interface/UI_Main/client/src/components/UIA.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './css/MasterCSS.css';
 import './css/UIACSS.css';
 import { useNavigate } from 'react-router-dom';
+import UIA_JSON from '../telemetry_json/UIA.json'
+import DCU_JSON from '../telemetry_json/DCU.json'
 
 /*
 * UIA() - **PAGE**
