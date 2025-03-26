67de594b780b025ca69fa77ebd753232bb955355
vicky04edx <victoria.herediac@gmail.com>
2025-03-26 18:16:47 -0500

    Updated Ingress page to look like egress page in preparation for upload

--- User_Interface/UI_Main/client/src/components/uia_subpages/Ingress.js
+++ User_Interface/UI_Main/client/src/components/uia_subpages/Ingress.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import DCU_Front from './../images/DCU_Front.jpg'
-import UIA_Panel from './../images/SUITS_UIA_PANEL.jpg'
+import SUITS_UIA_PANEL from "./../images/SUITS_UIA_PANEL.jpg";
 import './../css/MasterCSS.css'
 import './../css/UIACSS.css'
 import './../css/UIASubpageCSS.css'
+import Collapsible from "./../Collapsible";
+
 
 /*
-* Egress() - **PAGE**
+* Ingress() - **PAGE**
 *
 * Description:
 *      Page component where specifically ingress operations will
@@ -20,19 +22,66 @@ import './../css/UIASubpageCSS.css'
 */
 function Ingress() {
   return(
-    <div className='container'>
-      <div className='image-container'>
-        <div>
-          <img className='DCUFront_image' src={DCU_Front} alt='DCU'></img>
-        </div>
-      </div>
-      <div className='image-container'>
-        <div>
-          <img className='SUITS_UIA_PANEL_image' src={UIA_Panel} alt='UIA'></img>
-        </div>
-      </div>
 
-    </div>
+    
+    <div className="container">
+    <div className="text-container">
+     <h2>Ingress Operations</h2>
+     <p>
+       This section showcases the Ingress operations involved in our system. 
+     </p>
+
+
+     <Collapsible title="Pending steps">
+     <label className="checkbox-container">
+         <span>Step 1 </span>
+         <input type="checkbox" />
+         <span class="checkmark"></span>
+      </label>
+     </Collapsible> 
+
+
+     <Collapsible title="Pending steps">
+     <label className="checkbox-container">
+         <span>Step 1 </span>
+         <input type="checkbox" />
+         <span class="checkmark"></span>
+      </label>
+
+     </Collapsible>
+
+     <Collapsible title="Pending steps">
+     <label className="checkbox-container">
+         <span>Step 1 </span>
+         <input type="checkbox" />
+         <span class="checkmark"></span>
+      </label>
+     
+     </Collapsible>
+
+     <Collapsible title="Pending steps">
+
+     <label className="checkbox-container">
+         <span>Step 1 </span>
+         <input type="checkbox" />
+         <span class="checkmark"></span>
+      </label>
+
+     
+     </Collapsible>
+
+   </div>
+   
+   <div className="image-column">
+     <div className="image-container">
+       <img className="DCUFront_image" src={DCU_Front} alt="DCU Front" />
+     </div>
+
+     <div className="image-container">
+       <img className="SUITS_UIA_PANEL_image" src={SUITS_UIA_PANEL} alt="Panel" />
+     </div>
+   </div>
+ </div>
   )
 }
 
