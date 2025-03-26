7eac31d3b08b86a40db8afbef093737fc8ea2ff7
vicky04edx <victoria.herediac@gmail.com>
2025-03-26 18:06:01 -0500

    updated Egress page with customized checkboxes and changed colors

--- User_Interface/UI_Main/client/src/components/uia_subpages/Egress.js
+++ User_Interface/UI_Main/client/src/components/uia_subpages/Egress.js
@@ -25,36 +25,227 @@ function Egress() {
         <p>
           This section showcases the egress operations involved in our system. 
         </p>
-        <Collapsible title="Connect UIA to DCU and Start Depress">
-          <div className="checkbox-container">
-            <span>UIA and DCU 1. EV1 and EV2 connect UIA and DCU umbilical</span>
-            <input type="checkbox" />
-          </div>
 
-          <div className="checkbox-container">
+
+        <Collapsible title="Connect UIA to DCU and Start Depress">
+          <label className="checkbox-container">
+            <span>UIA and DCU 1. EV1 and EV2 connect UIA and DCU umbilical</span>
+            <input type="checkbox"/>
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
             <span>UIA 2. EV-1, EV-2 PWR – ON</span>
             <input type="checkbox" />
-          </div>
+            <span class="checkmark"></span>
+          </label>
 
-          <div className="checkbox-container">
+          <label className="checkbox-container">
             <span>BOTH DCU 3. BATT – UMB</span>
             <input type="checkbox" />
-          </div>
+            <span class="checkmark"></span>
+          </label>
 
-          <div className="checkbox-container">
+          <label className="checkbox-container">
             <span>UIA 4. DEPRESS PUMP PWR – ON</span>
             <input type="checkbox" />
-          </div>
+            <span class="checkmark"></span>
+          </label>
+        </Collapsible> 
+
+
+        <Collapsible title="Prep O2 Tanks">
+          <label className="checkbox-container">
+            <span>UIA 1. OXYGEN O2 VENT – OPEN </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span> HMD 2. Wait until both Primary and Secondary OXY tanks are less than 10psi</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>UIA 3. OXYGEN O2 VENT – CLOSE </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>BOTH DCU 4. OXY – PRI</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>UIA 5. OXYGEN EMU-1, EMU-2 – OPEN</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>HMD 6. Wait until EV1 and EV2 Primary O2 tanks greate than 3000 psi </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>UIA 7. OXYGEN EMU-1, EMU-2 – CLOSE </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>BOTH DCU 8. OXY – SEC</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>UIA 9. OXYGEN EMU-1, EMU-2 – OPEN</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>HMD 10. Wait until EV1 and EV2 Secondary O2 tanks greate than 3000 psi</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>UIA 11. OXYGEN EMU-1, EMU-2 – CLOSE </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <label className="checkbox-container">
+            <span>BOTH DCU 12. OXY – PRI</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
         </Collapsible>
 
+        <Collapsible title="Prep Water Tanks">
+         <label className="checkbox-container">
+            <span>BOTH DCU 1. PUMP – OPEN</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
 
-        <Collapsible title="Prep O2 Tanks">Content for section 2</Collapsible>
+         <label className="checkbox-container">
+            <span>UIA 2. EV-1, EV-2 WASTE WATER – OPEN</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
 
-        <Collapsible title="Prep Water Tanks">Content for section 3</Collapsible>
+         <label className="checkbox-container">
+            <span>HMD 3. Wait until water EV1 and EV2 Coolant tank is less than 5%</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
 
-        <Collapsible title="END Depress, Check Switches and Disconnect">Content for section 4</Collapsible>
+         <label className="checkbox-container">
+            <span>UIA 4. EV-1, EV-2 WASTE WATER – CLOSE</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>UIA 5. EV-1, EV-2 SUPPLY WATER – OPEN</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>HMD 6. Wait until water EV1 and EV2 Coolant tank is greater than 95%</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>UIA 7. EV-1, EV-2 SUPPLY WATER – CLOSE </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 8. PUMP – CLOSE</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+        
+        </Collapsible>
+
+        <Collapsible title="END Depress, Check Switches and Disconnect">
+
+        <label className="checkbox-container">
+            <span>HMD 1. Wait until SUIT P, O2 P = 4 </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>UIA 2. DEPRESS PUMP PWR – OFF </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 3. BATT – LOCAL </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>UIA 4. EV-1, EV-2 PWR - OFF </span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 5. Verify OXY – PRI</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 6. Verify COMMS – A</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 7. Verify FAN – PRI</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 8. Verify PUMP – CLOSE</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>BOTH DCU 9. Verify CO2 – A</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+
+         <label className="checkbox-container">
+            <span>UIA and DCU 10. EV1 and EV2 disconnect UIA and DCU umbilical</span>
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+         </label>
+        
+        </Collapsible>
 
       </div>
+      
       <div className="image-column">
         <div className="image-container">
           <img className="DCUFront_image" src={DCU_Front} alt="DCU Front" />
